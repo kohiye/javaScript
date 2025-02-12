4aca30cc3d8d3c8c2842fcb78a146be502d017b8
ivan <procciy@proton.me>
2025-02-12 18:56:43 +0700

    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+let str = "String";
+let num = 5;
+let none;
+let bool = true;
+let arr = [3,3,40];
+console.log(typeof(str));
+console.log(typeof(num));
+console.log(typeof(none));
+console.log(typeof(bool));
+console.log(typeof(arr));
