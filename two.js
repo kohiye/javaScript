49196ee4bf2308ff9e35d2abe2736f3be2f14768
ivan <procciy@proton.me>
2025-02-26 23:35:42 +0700

    one left in arrays

--- two.js
+++ two.js
@@ -111,3 +111,109 @@ let d = a.splice(1, 3);
 console.log(a);
 
 // 6)
+let nums = [1,2,3,4,5];
+console.log(nums.reverse());
+
+// 7)
+let stuff = ['c', 5, 2, 'b', 3, 1 ,4, 'a'];
+console.log(stuff.sort());
+
+// 8)
+console.log(nums.reverse().join('+'));
+
+// 9)
+let first = [1,2,5,4,6];
+let second = [8,2,5,9,5];
+let sorted = first.concat(second).sort();
+if (sorted.length % 2 == 0){
+    console.log((sorted[sorted.length/2] + sorted[sorted.length/2 + 1])/2);
+}else{
+    console.log(sorted[Math.floor(sorted.length/2)]);
+};
+
+// 10)
+arr = [1,5,1,0,-10,22,4];
+console.log(arr);
+max_index = 0;
+max = arr[0];
+min_index = 0;
+min = arr[0];
+for(i = 0; i < arr.length; ++i){
+    if(arr[i] < min){
+        min_index = i;
+        min = arr[i];
+    }else if(arr[i] > max){
+        max_index = i;
+        max = arr[i];
+    }
+}
+arr[max_index] = min;
+arr[min_index] = max;
+console.log(arr);
+
+// 11)
+function check(arr){
+    if(arr.length == 1){
+        console.log(arr);
+        return;
+    }
+    i = 1;
+    while(arr[i] < arr[i-1]){
+        if(i == arr.length){
+            console.log(arr.reverse());
+            return;
+        }
+        ++i;    
+    }
+    console.log(arr[i]);
+}
+
+// 12)
+function modify(arr){
+    for(i in arr){
+        if(i % 2 != 0 && arr[i] > 0){
+            arr[i] *= 3;
+        }else if(i % 2 == 0 && arr[i] < 0){
+            arr[i] *= 5;
+        }
+    }
+}
+
+// 13)
+let matrix = [[1,3,6,7,8], 
+              [3,5,6,7,7],
+              [2,-2,1,-1,0],
+              [1,10,-2,-6,-20],
+              [10,100,1000,50,0]];
+let flat = matrix.flat()
+for (i in flat){
+    if(flat[i] >= -5 && flat[i] <= 7){
+        console.log(flat[i]);
+    }
+}
+
+// 14)
+sum = 0;
+for(i=0; i<matrix.length; ++i){
+    max = matrix[i][0];
+    for(j=0; j<matrix[0].length; ++j){
+        if(matrix[i][j] > max)
+            max = matrix[i][j];
+    }
+    sum += max;
+}
+console.log(sum);
+product = 1;
+for(j=0; j < matrix[0].length; ++j){
+    min = matrix[0][j];
+    for(i=0; i < matrix.length; ++i){
+        if(matrix[i][j] < min)
+            min = matrix[i][j];
+    }
+    product *= min;
+}
+console.log(product);
+let dict = {"Pushkin" : "Eugene Onegin", "Pushkin" : "Boris Godunov", "Esenin": "Hooligan", "Dostoyevky": "Crime and punishment", "Dostoyevsky" : "The Idiot"};
+for (i in dict){
+    console.log(i);
+};
