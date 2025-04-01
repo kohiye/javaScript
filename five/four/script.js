0133be13f3a1aa071e70c0a5a0dcff919961622a
ivan <procciy@proton.me>
2025-04-01 21:11:11 +0700

    lab five

---
+++ five/four/script.js
@@ -0,0 +1,6 @@
+document.getElementById("age-table"); // table with id = "age-table"
+var list = document.querySelectorAll("#age-table label"); // all labels in the table
+var td = document.querySelectorAll("#age-table td")[0]; // first td
+var form = document.querySelector('form [name="search"]'); // search form
+var firstInput = document.querySelectorAll('form[name="search"] input')[0]; // search first input
+var secondInput = document.querySelectorAll('form[name="search"] input')[1]; // search second input
